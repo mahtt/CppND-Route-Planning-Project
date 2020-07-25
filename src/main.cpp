d8279ad84ef15f84581211a1bb5b740907e36efd
Martin <martinhuettinger.94@googlemail.com>
2020-07-25 13:26:24 +0200

    getting start and end from user input

--- src/main.cpp
+++ src/main.cpp
@@ -55,12 +55,33 @@ int main(int argc, const char **argv)
     // TODO 1: Declare floats `start_x`, `start_y`, `end_x`, and `end_y` and get
     // user input for these values using std::cin. Pass the user input to the
     // RoutePlanner object below in place of 10, 10, 90, 90.
+    float start_x, start_y, end_x, end_y;
+    start_x = 0;
+    start_y = 0;
+    end_x = 0;
+    end_y = 0;
+
+    std::cout << "Please enter the x-cooardinate of the starting position: ";
+    std::cin >> start_x;
+    std::cout << "\n";
+    std::cout << "Please enter the y-cooardinate of the starting position: ";
+    std::cin >> start_y;
+    std::cout << "\n";
+
+    std::cout << "Please enter the x-cooardinate of the end position: ";
+    std::cin >> end_x;
+    std::cout << "\n";
+    std::cout << "Please enter the y-cooardinate of the end position: ";
+    std::cin >> end_y;
+    std::cout << "\n";
+
+
 
     // Build Model.
     RouteModel model{osm_data};
 
     // Create RoutePlanner object and perform A* search.
-    RoutePlanner route_planner{model, 10, 10, 90, 90};
+    RoutePlanner route_planner{model, start_x, start_y, end_x, end_y};
     route_planner.AStarSearch();
 
     std::cout << "Distance: " << route_planner.GetDistance() << " meters. \n";
@@ -76,4 +97,5 @@ int main(int argc, const char **argv)
         render.Display(surface);
     });
     display.begin_show();
+    
 }
